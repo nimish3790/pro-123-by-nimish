cb1e16d7a7444b97fae35df343b124bfa60ab451
nimish3790 <75606319+nimish3790@users.noreply.github.com>
2022-05-07 09:58:52 +0530

    Add files via upload

---
+++ digit.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np 
+import pandas as pd 
+import seaborn as sns 
+import matplotlib.pyplot as plt 
+from sklearn.datasets import fetch_openml
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from PIL import Image
+import PIL,ImageOps
+import os,ssl,time
+
+X,y = fetch_openml('mnist_784',version = 1,return_X_y = True)
+print(pd.Series(y).value_counts())
+classes = ['0','1','2','3','4','5','6','7','8','9']
+nclasses = len(classes)
+
+X_train,X_test,y_train,y_test = train_test_split(X,y,random_state=9,train_size = 7500,test_size = 2500)
+X_train_scaled = X_train/255.0
+X_test_scaled = X_test/255.0
+
+clf = LogisticRegression(solver = 'saga',multi_class = 'multinomial').fit(X_train_scaled,y_train)
+
+y_pred = clf.predict(X_test_scaled)
+accuracy = accuracy_score(y_test,y_pred)
+print('the accuracy is',accuracy)
